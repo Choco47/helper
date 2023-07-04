350a70f330d39611ab430f1b97431087284704f5
Choco47 <87600745+Choco47@users.noreply.github.com>
2023-07-04 10:36:03 +0800

    Create 2 Examples of setting Field List ID in single line & set margin-bottom.jsx

---
+++ 2 Examples of setting Field List ID in single line & set margin-bottom.jsx
@@ -0,0 +1,24 @@
+//Example 1:
+
+<style>{`
+[id="signer_1_applicant.fieldlist_1_comp"]>fieldset>legend {
+    width: 40px !important;
+}
+[id="signer_1_applicant.listOfFields"]>fieldset>:nth-child(2)>:nth-child(1){
+    margin-bottom: 20px !important;
+}
+`}</style>
+
+
+//Example 2:
+
+const style = `
+[id="signer_1_applicant.fieldlist_1_comp"]>fieldset>legend {
+    width: 40px !important;
+}
+[id="signer_1_applicant.listOfFields"]>fieldset>:nth-child(2)>:nth-child(1){
+    margin-bottom: 20px !important;
+}
+`;
+
+<style>{style}</style>
