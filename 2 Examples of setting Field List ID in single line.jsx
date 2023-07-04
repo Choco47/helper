b6f11b070ab266963d6ba83ea17699d6111fd1d0
Choco47 <87600745+Choco47@users.noreply.github.com>
2023-07-04 10:34:58 +0800

    Create 2 Examples of setting Field List ID in single line.jsx

---
+++ 2 Examples of setting Field List ID in single line.jsx
@@ -0,0 +1,18 @@
+Example 1:
+
+<style>{`
+[id="signer_1_applicant.fieldlist_1_comp"]>fieldset>legend {
+    width: 40px !important;
+}
+`}</style>
+
+
+Example 2:
+
+const style = `
+[id="signer_1_applicant.fieldlist_1_comp"]>fieldset>legend {
+    width: 40px !important;
+}
+`;
+
+<style>{style}</style>
