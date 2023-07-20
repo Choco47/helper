cd599a2dade8e92c9b19eab0cc78c084343cf982
Choco47 <87600745+Choco47@users.noreply.github.com>
2023-07-20 09:14:35 +0800

    Create fsd_list_arr.py

---
+++ fsd_list_arr.py
@@ -0,0 +1,292 @@
+fsd_list = [
+    # Hong Kong Fire Commend
+    ["Central Fire Station", "cjp_itmu_5@hkfsd.gov.hk"],
+    ["Kotewall Fire Station", ""],
+    ["Kong Wan Fire Station", ""],
+    ["Sheung Wan Fire Station", ""],
+    ["Victoria Peak Fire Station", ""],
+    ["Wan Chai Fire Station", ""],
+    ["Braemar Hill Fire Station", ""],
+    ["Chai Wan Fire Station", ""],
+    ["North Point Fire Station", ""],
+    ["Sai Wan Ho Fire Station", ""],
+    ["Shau Kei Wan Fire Station", ""],
+    ["Tung Lo Wan Fire Station", ""],
+    ["Aberdeen Fire Station", ""],
+    ["Ap Lei Chau Fire Station", ""],
+    ["Chung Hom Kok Fire Station", ""],
+    ["Kennedy Town Fire Station", ""],
+    ["Pok Fu Lam Fire Station", ""],
+    ["Sandy Bay Fire Station", ""],
+    ["Aberdeen Fireboat Station", ""],
+    ["Central Fireboat Station", ""],
+    ["Cheung Chau Fire Station", ""],
+    ["Cheung Chau Fireboat Station", ""],
+    ["Cheung Sha Fire Station", ""],
+    ["Discovery Bay Fire Station", ""],
+    ["Lamma Fire Station", ""],
+    ["Mui Wo Fire Station", ""],
+    ["North Point Fireboat Station", ""],
+    ["Peng Chau Fire Station", ""],
+    ["Tai O (Old) Fire Station", ""],
+    ["Tai O Fire Station", ""],
+    ["Tsing Yi Fireboat Station", ""],
+    ["Tuen Mun Fireboat Station", ""],
+    # <----------------------------->
+    # Kowloon Command
+    ["Kai Tak Fire Station", ""],
+    ["Ma Tau Chung Fire Station", ""],
+    ["Ngau Chi Wan Fire Station", ""],
+    ["Sai Kung Fire Station", ""],
+    ["Shun Lee Fire Station", ""],
+    ["Wong Tai Sin Fire Station", ""],
+    ["Kowloon Bay Fire Station", ""],
+    ["Kwun Tong Fire Station", ""],
+    ["Lam Tin Fire Station", ""],
+    ["Po Lam Fire Station", ""],
+    ["Tai Chik Sha Fire Station", ""],
+    ["Yau Tong Fire Station", ""],
+    ["Hung Hom Fire Station", ""],
+    ["Tsim Sha Tsui Fire Station", ""],
+    ["Tsim Tung Fire Station", ""],
+    ["Yau Ma Tei Fire Station", ""],
+    ["Cheung Sha Wan Fire Station", ""],
+    ["Kowloon Tong Fire Station", ""],
+    ["Lai Chi Kok Fire Station", ""],
+    ["Mongkok Fire Station", ""],
+    ["Shek Kip Mei Fire Station", ""],
+
+    # <------------------------------>
+    # New Territories South Command
+
+    ["Airport South Fire Station", ""],
+    ["Airport Centre Fire Station", ""],
+    ["Airport North Fire Station", ""],
+    ["East Sea Rescue Berth", ""],
+    ["West Sea Rescue Berth", ""],
+    ["Kwai Chung Fire Station", ""],
+    ["Lai King Fire Station", ""],
+    ["Lei Muk Shue Fire Station", ""],
+    ["Sham Tseng Fire Station", ""],
+    ["Tsuen Wan Fire Station", ""],
+    ["Chek Lap Kok South Fire Station", ""],
+    ["Ma Wan Fire Station", ""],
+    ["Penny's Bay Fire Station", ""],
+    ["Tsing Yi Fire Station", ""],
+    ["Tsing Yi South Fire Station", ""],
+    ["Tung Chung Fire Station", ""],
+    ["Hong Kong-Zhuhai-Macao Bridge Fire Station", ""],
+
+
+    # <------------------------------>
+    # New Territories North Command
+
+    ["Ma On Shan Fire Station", ""],
+    ["Sha Tin Fire Station", ""],
+    ["Siu Lek Yuen Fire Station", ""],
+    ["Tai Po East Fire Station", ""],
+    ["Tai Po Fire Station", ""],
+    ["Tai Sum Fire Station", ""],
+    ["Fanling Fire Station", ""],
+    ["Mai Po Fire Station", ""],
+    ["Pat Heung Fire Station", ""],
+    ["Sha Tau Kok Fire Station", ""],
+    ["Sheung Shui Fire Station", ""],
+    ["Heung Yuen Wai Fire Station", ""],
+    ["Yuen Long Fire Station", ""],
+    ["Castle Peak Bay Fire Station", ""],
+    ["Fu Tei Fire Station", ""],
+    ["Lau Fau Shan Fire Station", ""],
+    ["Pillar Point Fire Station", ""],
+    ["Shenzhen Bay Fire Station", ""],
+    ["Tai Lam Chung Fire Station", ""],
+    ["Tin Shui Wai Fire Station", ""],
+    ["Tuen Mun Fire Station", ""],
+
+    # <------------------------------>
+    # <------------------------------>
+    # <------------------------------>
+    # Ambulance Depot
+    # <------------------------------>
+
+    # Hong Kong and Kowloon Region
+    ["Braemar Hill Ambulance Depot", ""],
+    ["Chai Wan Ambulance Station", ""],
+    ["Morrison Hill Ambulance Depot", ""],
+    ["Sai Wan Ho Ambulance Depot", ""],
+    ["Aberdeen Ambulance Depot", ""],
+    ["Mount Davis Ambulance Depot", ""],
+    ["Pok Fu Lam Ambulance Depot", ""],
+    ["Lam Tin Ambulance Depot", ""],
+    ["Ngau Tau Kok Ambulance Depot", ""],
+    ["Po Lam Ambulance Depot", ""],
+    ["Tai Chik Sha Ambulance Depot", ""],
+    ["Wong Tai Sin Ambulance Depot", ""],
+    ["Ho Man Tin Ambulance Depot", ""],
+    ["Kowloon Tong Ambulance Depot", ""],
+    ["Ma Tau Chung Ambulance Depot", ""],
+    ["Pak Tin Ambulance Depot", ""],
+    ["Cheung Sha Wan Ambulance Depot", ""],
+    ["Lai Chi Kok Ambulance Depot", ""],
+    ["Mongkok Ambulance Depot", ""],
+    ["Tsim Tung Ambulance Depot", ""],
+    ["Yau Ma Tei Ambulance Depot", ""],
+
+    # New Territories Region
+    ["Ta Kwu Ling Ambulance Depot", ""],
+    ["Fanling Ambulance Depot", ""],
+    ["Lau Fau Shan Ambulance Depot", ""],
+    ["Sheung Shui Ambulance Depot", ""],
+    ["Tai Po Ambulance Depot", ""],
+    ["Tin Shui Wai Ambulance Depot", ""],
+    ["Castle Peak Bay Ambulance Depot", ""],
+    ["Sham Tseng Ambulance Depot", ""],
+    ["Tuen Mun Ambulance Depot", ""],
+    ["Yuen Long Ambulance Depot", ""],
+    ["Lei Muk Shue Ambulance Depot", ""],
+    ["Ma On Shan Ambulance Depot", ""],
+    ["Sha Tin Ambulance Depot", ""],
+    ["Tin Sum Ambulance Depot", ""],
+    ["Kwai Chung Ambulance Depot", ""],
+    ["Penny's Bay Ambulance Depot", ""],
+    ["Tsing Yi Ambulance Depot", ""],
+    ["Tsuen Wan Ambulance Depot", ""],
+    ["Tung Chung Ambulance Depot", ""],
+    ["Hong Kong-Zhuhai-Macao Bridge Ambulance Depot", ""]
+]
+
+https://tel.directory.gov.hk/0218005037_CHI.html
+
+"Central Fire Station", "c_fstn@hkfsd.gov.hk", "Mr. CHOI Ka Wing", 2522 0367
+"Kotewall Fire Station", "ko_fstn@hkfsd.gov.hk", "Mr. HUNG Hing Chiu", 2858 4803
+"Kong Wan Fire Station", "kw_fstn@hkfsd.gov.hk", "Mr. MAN Fu Kong", 2802 0947
+"Sheung Wan Fire Station", "sw_fstn@hkfsd.gov.hk", "Mr. LAU Ying Kai", 2559 2417
+"Victoria Peak Fire Station", "ko_fstn@hkfsd.gov.hk", "Mr. HUNG Hing Chiu", 2858 4803
+"Wan Chai Fire Station", "wch_fstn@hkfsd.gov.hk", "Mr. HO Chun Pang", 2891 2587
+"Braemar Hill Fire Station", "bh_fstn@hkfsd.gov.hk", "Mr. LI Tsz Chun", 2571 5063
+"Chai Wan Fire Station", "cw_fstn@hkfsd.gov.hk", "Mr. CHEUNG Wing Hei", 2558 7178
+"North Point Fire Station", "np_fstn@hkfsd.gov.hk", "Mr. LEUNG Kai Chuen", 2561 5260
+"Sai Wan Ho Fire Station", "swh_fstn@hkfsd.gov.hk", "Mr. SHIN Kar Tsai", 2569 3165
+"Shau Kei Wan Fire Station", "skw_fstn@hkfsd.gov.hk", "Mr. TSANG Chiu Lok", 2884 3197
+"Tung Lo Wan Fire Station", "tlw_fstn@hkfsd.gov.hk", "Mr. CHIU Wing Fai", 2571 8760
+"Aberdeen Fire Station", "abd_fstn@hkfsd.gov.hk", "Mr. YAM Ka Wang", 2552 5280
+"Ap Lei Chau Fire Station", "alc_fstn@hkfsd.gov.hk", "Mr. WONG Yiu Man", 2814 8836
+"Chung Hom Kok Fire Station", "chk_fstn@hkfsd.gov.hk", "Mr. TSANG Wai Ming", 2813 2496
+"Kennedy Town Fire Station", "k_fstn@hkfsd.gov.hk", "Mr. NG Siu Ming", 2818 7046
+"Pok Fu Lam Fire Station", "pfl_fstn@hkfsd.gov.hk", "Mr. CHEUNG Pit Shing", 2551 8301
+"Sandy Bay Fire Station", "sb_fstn@hkfsd.gov.hk", "Mr. NG Kin Man", 2904 9672
+"Aberdeen Fireboat Station", "", "", 
+"Central Fireboat Station", "", "", 
+"Cheung Chau Fire Station", "", "", 
+"Cheung Chau Fireboat Station", "", "", 
+"Cheung Sha Fire Station", "", "", 
+"Discovery Bay Fire Station", "", "", 
+"Lamma Fire Station", "", "", 
+"Mui Wo Fire Station", "", "", 
+"North Point Fireboat Station", "", "", 
+"Peng Chau Fire Station", "", "", 
+"Tai O (Old) Fire Station", "", "", 
+"Tai O Fire Station", "", "", 
+"Tsing Yi Fireboat Station", "", "", 
+"Tuen Mun Fireboat Station", "", "", 
+"Kai Tak Fire Station", "", "", 
+"Ma Tau Chung Fire Station", "mtc_fstn@hkfsd.gov.hk", "Mr. NG Chi Chiu", 2711 0292
+"Ngau Chi Wan Fire Station", "ncw_fstn@hkfsd.gov.hk", "Mr. LUKE Shung Chi, Stephen", 2320 0292
+"Sai Kung Fire Station", "", "", 
+"Shun Lee Fire Station", "sl_fstn@hkfsd.gov.hk", "Mr. YIM Ying Kit", 2341 9420
+"Wong Tai Sin Fire Station", "wts_fstn@hkfsd.gov.hk", "Mr. TUNG Wing Kei", 2329 5733
+"Kowloon Bay Fire Station", "", "", 
+"Kwun Tong Fire Station", "", "", 
+"Lam Tin Fire Station", "", "", 
+"Po Lam Fire Station", "", "", 
+"Tai Chik Sha Fire Station", "", "", 
+"Yau Tong Fire Station", "", "", 
+"Hung Hom Fire Station", "", "", 
+"Tsim Sha Tsui Fire Station", "", "", 
+"Tsim Tung Fire Station", "", "", 
+"Yau Ma Tei Fire Station", "", "", 
+"Cheung Sha Wan Fire Station", "", "", 
+"Kowloon Tong Fire Station", "", "", 
+"Lai Chi Kok Fire Station", "", "", 
+"Mongkok Fire Station", "", "", 
+"Shek Kip Mei Fire Station", "", "", 
+"Airport South Fire Station", "", "", 
+"Airport Centre Fire Station", "", "", 
+"Airport North Fire Station", "", "", 
+"East Sea Rescue Berth", "", "", 
+"West Sea Rescue Berth", "", "", 
+"Kwai Chung Fire Station", "", "", 
+"Lai King Fire Station", "", "", 
+"Lei Muk Shue Fire Station", "", "", 
+"Sham Tseng Fire Station", "", "", 
+"Tsuen Wan Fire Station", "", "", 
+"Chek Lap Kok South Fire Station", "", "", 
+"Ma Wan Fire Station", "", "", 
+"Penny's Bay Fire Station", "", "", 
+"Tsing Yi Fire Station", "", "", 
+"Tsing Yi South Fire Station", "", "", 
+"Tung Chung Fire Station", "", "", 
+"Hong Kong-Zhuhai-Macao Bridge Fire Station", "", "", 
+"Ma On Shan Fire Station", "", "", 
+"Sha Tin Fire Station", "", "", 
+"Siu Lek Yuen Fire Station", "", "", 
+"Tai Po East Fire Station", "", "", 
+"Tai Po Fire Station", "", "", 
+"Tai Sum Fire Station", "", "", 
+"Fanling Fire Station", "", "", 
+"Mai Po Fire Station", "", "", 
+"Pat Heung Fire Station", "", "", 
+"Sha Tau Kok Fire Station", "", "", 
+"Sheung Shui Fire Station", "", "", 
+"Heung Yuen Wai Fire Station", "", "", 
+"Yuen Long Fire Station", "", "", 
+"Castle Peak Bay Fire Station", "", "", 
+"Fu Tei Fire Station", "", "", 
+"Lau Fau Shan Fire Station", "", "", 
+"Pillar Point Fire Station", "", "", 
+"Shenzhen Bay Fire Station", "", "", 
+"Tai Lam Chung Fire Station", "", "", 
+"Tin Shui Wai Fire Station", "", "", 
+"Tuen Mun Fire Station", "", "", 
+"Braemar Hill Ambulance Depot", "bh_ambdep@hkfsd.gov.hk", "Mr. CHUONG Kok Fai", 2571 5293
+"Chai Wan Ambulance Station", "cw_ambdep@hkfsd.gov.hk", "Mr. CHAN Howin", 2556 6490
+"Morrison Hill Ambulance Depot", "mh_ambdep@hkfsd.gov.hk", "Mr. LEUNG Hoi Kei", 2893 5592
+"Sai Wan Ho Ambulance Depot", "swh_ambdep@hkfsd.gov.hk", "Mr. LO Cho Kwun", 2569 7627
+"Aberdeen Ambulance Depot", "abd_ambdep@hkfsd.gov.hk", "Mr. YUEN Yiu Leung", 2541 6644
+"Mount Davis Ambulance Depot", "md_ambdep@hkfsd.gov.hk", "Mr. SHUM Ka Yam", 2819 7967
+"Pok Fu Lam Ambulance Depot", "pfl_ambdep@hkfsd.gov.hk", "Mr. LAU Siu Fai", 2551 6000
+"Lam Tin Ambulance Depot", "lt_ambdep@hkfsd.gov.hk", "Mr. WONG Po Lung", 2952 3861
+"Ngau Tau Kok Ambulance Depot", "ntk_ambdep@hkfsd.gov.hk", "Mr. YUEN Chung Ho", 2750 1710
+"Po Lam Ambulance Depot", "pl_ambdep@hkfsd.gov.hk", "Mr. LEUNG Wai Ho", 2701 0955
+"Tai Chik Sha Ambulance Depot", "tcs_ambdep@hkfsd.gov.hk", "Mr. LEUNG Wai Ho", 2623 9232
+"Wong Tai Sin Ambulance Depot", "wts_ambdep@hkfsd.gov.hk", "Mr. CHAN Wai Chun", 2323 9949
+"Ho Man Tin Ambulance Depot", "hmt_ambdep@hkfsd.gov.hk", "Mr. CHAN Cheong Fai", 2624 6110
+"Kowloon Tong Ambulance Depot", "klt_ambdep@hkfsd.gov.hk", "Mr. CHAN Ka Ki", 3793 6499
+"Ma Tau Chung Ambulance Depot", "mtc_ambdep@hkfsd.gov.hk", "Mr. LEUNG Wing Shun", 2714 3746
+"Pak Tin Ambulance Depot", "pt_ambdep@hkfsd.gov.hk", "Mr. CHEUNG Yung", 2776 7803
+"Cheung Sha Wan Ambulance Depot", "csw_ambdep@hkfsd.gov.hk", "Mr. KWOK King Hin", 2708 1573
+"Lai Chi Kok Ambulance Depot", "lck_ambdep@hkfsd.gov.hk", "Mr. CHUNG Tim Kwai", 2959 1857
+"Mongkok Ambulance Depot", "mk_ambdep@hkfsd.gov.hk", "Mr. SIU Ming Chun", 2117 6570
+"Tsim Tung Ambulance Depot", "tt_ambdep@hkfsd.gov.hk", "Mr. WONG Chun Kin", 2311 5111
+"Yau Ma Tei Ambulance Depot", "ymt_ambdep@hkfsd.gov.hk", "Mr. WONG Ka Chun", 2770 2547
+"Ta Kwu Ling Ambulance Depot", "tkl_ambdep@hkfsd.gov.hk", "Mr. AU YEUNG Wai kit", 2756 3895
+"Fanling Ambulance Depot", "fl_ambdep@hkfsd.gov.hk", "Mr. LAI Hong Keung", 2669 2250
+"Lau Fau Shan Ambulance Depot", "lfs_ambdep@hkfsd.gov.hk", "Mr. LIU Kay Yip", 2446 2892
+"Sheung Shui Ambulance Depot", "ss_ambdep@hkfsd.gov.hk", "Mr. CHUNG Wai Wo", 2671 1271
+"Tai Po Ambulance Depot", "tp_ambdep@hkfsd.gov.hk", "Mr. WONG Man Wai", 2658 0359
+"Tin Shui Wai Ambulance Depot", "tsw_ambdep@hkfsd.gov.hk", "Mr. PUN Kong Chak", 2445 0033
+"Castle Peak Bay Ambulance Depot", "cpb_ambdep@hkfsd.gov.hk", "Mr. LAM King Ki", 2451 7170
+"Sham Tseng Ambulance Depot", "smt_ambdep@hkfsd.gov.hk", "Mr. LAM King Ki", 2496 4917
+"Tuen Mun Ambulance Depot", "tm_ambdep@hkfsd.gov.hk", "Mr. TAM Ngo Yin, Vincent", 2463 2125
+"Yuen Long Ambulance Depot", "yl_ambdep@hkfsd.gov.hk", "Mr. LAM Ho Cheung, Lawrence", 2474 6845
+"Lei Muk Shue Ambulance Depot", "lms_ambdep@hkfsd.gov.hk", "Mr. HUI Sze Choi", 2487 1957
+"Ma On Shan Ambulance Depot", "mos_ambdep@hkfsd.gov.hk", "Mr. SIU King Fai", 2640 3619
+"Sha Tin Ambulance Depot", "st_ambdep@hkfsd.gov.hk", "Mr. YEUNG Keung Sang", 2691 2514
+"Tin Sum Ambulance Depot", "ts_ambdep@hkfsd.gov.hk", "Mr. CHAN Hiu Fat", 2681 0713
+"Kwai Chung Ambulance Depot", "kc_ambdep@hkfsd.gov.hk", "Mr. CHAN Wai Cheong", 2417 5750
+"Penny's Bay Ambulance Depot", "pb_ambdep@hkfsd.gov.hk", "Mr. YEUNG Pui Hoi", 2983 6541
+"Tsing Yi Ambulance Depot", "ty_ambdep@hkfsd.gov.hk", "Mr. YEUNG Pui Hoi", 2495 5080
+"Tsuen Wan Ambulance Depot", "tw_ambdep@hkfsd.gov.hk", "Mr. CHAN Ting Kwan", 2416 2751
+"Tung Chung Ambulance Depot", "tc_ambdep@hkfsd.gov.hk", "Mr. AU YEUNG Wang Kin", 2988 8282
+"Hong Kong-Zhuhai-Macao Bridge Ambulance Depot", "hzmb_ambdep@hkfsd.gov.hk", "Mr. MOK Siu Chung", 2516 0205
+
